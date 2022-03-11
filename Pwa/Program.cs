88add5cbe116e5f9f6bd40c8c095186852111d7d
Pawel Galezowski <pgalezowski@gmail.com>
2022-03-12 00:31:41 +0100

    cleanups

--- Pwa/Program.cs
+++ Pwa/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Pwa;
+using Microsoft.AspNetCore.Components;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+
+builder.RootComponents.Add<Galnet.Web.Base.GalnetApp>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddGalnetWebBase(
+    typeof(Program).Assembly,
+    new List<System.Reflection.Assembly>
+    {
+        typeof(Galnet.Web.Pages.Details).Assembly
+    });
 
 await builder.Build().RunAsync();

d9e2aab33c9735b35e4f833df104c6b97929d8d8
Pawel Galezowski <pgalezowski@gmail.com>
2023-03-05 20:23:30 +0100

    file clenaiups

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,9 @@
+using Logic;
 using Microsoft.AspNetCore.Components;
+using Pages;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
@@ -8,11 +11,32 @@ builder.Services.AddGalnetWebBase(
     typeof(Galnet.Web.Base.GalnetApp).Assembly,
     new List<System.Reflection.Assembly>
     {
-        typeof(Pages.Details).Assembly,
+        typeof(Details).Assembly,
+    },
+    new GalnetWebConfig
+    {
+        ShowTopMenu = true,
+        //TopMenu = typeof(CustomTopMenu)
+    }, 
+    withLocalPlatformStatusPlugin: true,    
+    pluginInitialization: (ps) =>
+    {
+        //ps.AddAuthenticationPlugins();
+        //ps.AddFacebookAuthenticationPlugins();
+
+        ps.AddPlugin("TestWaiter", (ctx) =>
+        {
+            Thread.Sleep(10000);
+        });
+
+        new Service().Register(ps);
     });
 
-var app = builder.Build();
+//builder.Services.AddGalnetAuthentication();
+//builder.Services.AddGalnetGoogleButton();
+//builder.Services.AddGalnetFacebookButton();
 
+var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
@@ -23,6 +47,16 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.MapBlazorHub();
-app.MapFallbackToPage("/_Root");
+app.MapFallbackToPage("/_RootWithGoogleButton");
+//app.MapFallbackToPage("/_Root");
+
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.MapGet("/galnetapi/hi", () => "Hello World!");
 
 app.Run();
